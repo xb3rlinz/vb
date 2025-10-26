4eaad8cae30637a6cb4e04fded7c2b5e1ad6b8f5
Berlin <xb3rlinz@gmail.com>
2025-10-26 11:11:38 +0700

    Update tm-test.user.js

--- tm-test.user.js
+++ tm-test.user.js
@@ -9,7 +9,7 @@
 // @run-at       document-start
 // @grant        unsafeWindow
 // @icon         https://i.imgur.com/wY2kL.jpeg
-// @downloadURL  https://github.com/xb3rlinz/vb/raw/main/tm-test.user.js
-// @updateURL    https://github.com/xb3rlinz/vb/raw/main/tm-tset.user.js
+// @downloadURL  https://raw.githubusercontent.com/xb3rlinz/vb/125a09161a719c17291bc9344f386c2cd5ccdbe5/tm-test.user.js
+// @updateURL    https://raw.githubusercontent.com/xb3rlinz/vb/125a09161a719c17291bc9344f386c2cd5ccdbe5/tm-test.user.js
 // @require      https://raw.githubusercontent.com/xb3rlinz/vb/refs/heads/main/test.js
 // ==/UserScript==
