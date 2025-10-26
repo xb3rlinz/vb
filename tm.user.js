f00997fe6f65d3acb186e251f72e53f6d5f26ba3
Berlin <xb3rlinz@gmail.com>
2025-10-26 11:07:07 +0700

    Update tm.user.js

--- tm.user.js
+++ tm.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Volcano
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  no desc
 // @author       Berlin
 // @match        https://key.volcano.wtf/*
