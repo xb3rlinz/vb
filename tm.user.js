8601f93cc0602abb7b26391eb877f0fd91e35ab2
Berlin <xb3rlinz@gmail.com>
2025-10-26 10:42:12 +0700

    Update tm.user.js

--- tm.user.js
+++ tm.user.js
@@ -9,7 +9,7 @@
 // @run-at       document-start
 // @grant        unsafeWindow
 // @icon         https://i.imgur.com/wY2kL.jpeg
-// @downloadURL  https://raw.githubusercontent.com/xb3rlinz/vb/6c7077c785b98abc702f71345ae62b11f9bd6803/tm.user.js
-// @updateURL    https://raw.githubusercontent.com/xb3rlinz/vb/6c7077c785b98abc702f71345ae62b11f9bd6803/tm.user.js
+// @downloadURL  https://github.com/xb3rlinz/vb/raw/main/tm.user.js
+// @updateURL    https://github.com/xb3rlinz/vb/raw/main/tm.user.js
 // @require      https://raw.githubusercontent.com/xb3rlinz/vb/refs/heads/main/bypasser.js
 // ==/UserScript==
