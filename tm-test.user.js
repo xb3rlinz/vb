61151e75bad475767e75bec8259f138bd521b17d
Berlin <xb3rlinz@gmail.com>
2025-10-26 11:08:29 +0700

    Create tm-test.user.js

---
+++ tm-test.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Volcano
+// @namespace    http://tampermonkey.net/
+// @version      1.1
+// @description  no desc
+// @author       Berlin
+// @match        https://key.volcano.wtf/*
+// @match        https://work.ink/*
+// @run-at       document-start
+// @grant        unsafeWindow
+// @icon         https://i.imgur.com/wY2kL.jpeg
+// @downloadURL  https://github.com/xb3rlinz/vb/raw/main/tm.user.js
+// @updateURL    https://github.com/xb3rlinz/vb/raw/main/tm.user.js
+// @require      https://raw.githubusercontent.com/xb3rlinz/vb/refs/heads/main/test.js
+// ==/UserScript==
