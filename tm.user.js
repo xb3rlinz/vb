6c7077c785b98abc702f71345ae62b11f9bd6803
Berlin <xb3rlinz@gmail.com>
2025-10-26 10:39:56 +0700

    Update and rename tm.js to tm.user.js

--- tm.user.js
+++ tm.user.js
@@ -9,7 +9,7 @@
 // @run-at       document-start
 // @grant        unsafeWindow
 // @icon         https://i.imgur.com/wY2kL.jpeg
-// @downloadURL  https://raw.githubusercontent.com/xb3rlinz/vb/refs/heads/main/user.js
-// @updateURL    https://raw.githubusercontent.com/xb3rlinz/vb/refs/heads/main/user.js
+// @downloadURL  https://raw.githubusercontent.com/xb3rlinz/vb/ddd4aa1af513b4442c53ed105c6ed209412c9765/user.js
+// @updateURL    https://raw.githubusercontent.com/xb3rlinz/vb/ddd4aa1af513b4442c53ed105c6ed209412c9765/user.js
 // @require      https://raw.githubusercontent.com/xb3rlinz/vb/refs/heads/main/bypasser.js
 // ==/UserScript==
