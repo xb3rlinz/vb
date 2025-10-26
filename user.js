ddd4aa1af513b4442c53ed105c6ed209412c9765
Berlin <xb3rlinz@gmail.com>
2025-10-26 10:04:09 +0700

    Create user.js

---
+++ user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Volcano
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  no desc
+// @author       Berlin
+// @match        https://key.volcano.wtf/*
+// @match        https://work.ink/*
+// @run-at       document-start
+// @grant        unsafeWindow
+// @icon         https://i.imgur.com/wY2kL.jpeg
+// @downloadURL  
+// @updateURL    
+// @require      https://raw.githubusercontent.com/xb3rlinz/vb/3e90ec13bc05c4d4a0c6c9da9e49c3bbd04dc189/bypasser.js
+// ==/UserScript==
